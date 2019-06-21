92b9a6386aa0d87d40a6f02ed639f0cb06320550
Fabio William Conceição <messhias@gmail.com>
2019-06-21 12:18:35 +0400

    updates

---
+++ backend/precache-manifest.1b422ea158db510eece27cbe24bd233e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d8e0e53d5db9e8da69782012af9cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f0102fae32bc157d92f",
+    "url": "/static/css/2.7e1a6a2f.chunk.css"
+  },
+  {
+    "revision": "d182c2426b5e4f8580b5",
+    "url": "/static/css/main.a2360636.chunk.css"
+  },
+  {
+    "revision": "2f0102fae32bc157d92f",
+    "url": "/static/js/2.50892c54.chunk.js"
+  },
+  {
+    "revision": "d182c2426b5e4f8580b5",
+    "url": "/static/js/main.79ca3080.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
