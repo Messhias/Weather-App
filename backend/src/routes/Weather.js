26d94a2b05849df0a87e93c107d5df13d83b22fb
Fabio William Conceição <messhias@gmail.com>
2019-06-22 02:02:02 +0400

    fixed fixed node backend database issues in the server

--- backend/src/routes/Weather.js
+++ backend/src/routes/Weather.js
@@ -1,16 +1,17 @@
 import {__construct} from "../utils/initApp";
 
 const WeatherRouter = express();
-
 import express from "express";
 import cors from 'cors';
+
 // Parse URL-encoded bodies (as sent by HTML forms)
 WeatherRouter.use(express.urlencoded({extended: true }));
 WeatherRouter.use(cors());
+
 // Parse JSON bodies (as sent by API clients)
 WeatherRouter.use(express.json());
 
-// importing the custom functions.
+// importing JWT functions.
 import {verifyJWT } from "../utils/JWT";
 
 const { Client } = require('pg');
@@ -27,7 +28,8 @@ WeatherRouter.post('/weather', verifyJWT, (request, response, next) => {
     __construct();
     const query = "select * from locations_data where country = $1 and city = $2 limit 1";
     client.query(query, [`${request.body.country}`, `${request.body.city}`])
-        .then(res => response.status(200).send(res.rows[0]));
+        .then(res => response.status(200).send(res.rows[0]))
+        .catch(e => console.error(e));
 });
 
 module.exports = WeatherRouter;
